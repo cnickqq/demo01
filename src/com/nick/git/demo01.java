fbb5b5720408367430ccaaa7829daee962916051
wushangguo <wushangguo@zsoft.com.cn>
2019-04-28 16:43:56 +0800

    新增语句System.out.println("Hello Git02");

--- src/com/nick/git/demo01.java
+++ src/com/nick/git/demo01.java
@@ -2,6 +2,7 @@ package com.nick.git;
 
 public class demo01 {
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
+		System.out.println("Hello Git01");
+		System.out.println("Hello Git02");
 	}
 }
